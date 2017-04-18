9764135e35169da683997b5097bca10ec8cf413c
chikara <chenhongtu@51xianqu.net>
2017-04-18 10:12:40 +0800

    feat: cli for clean

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ yargs
   .usage('react-power <cmd> [args]')
   .command(
     'dev',
-    'Run in development environment',
+    'Run in development environment.',
     () => {},
     argv => {
       require('../lib/dev');
@@ -14,10 +14,27 @@ yargs
   )
   .command(
     'build',
-    'Run in production environment',
-    () => {},
+    'Run in production environment.',
+    yargs => yargs.option('watch', {
+      alias: 'w',
+      default: false
+    }),
     argv => {
+      process._argv = argv
       require('../lib/build');
     }
   )
+  .command(
+    'clean',
+    'Clean the work directory.',
+    () => {},
+    argv => {
+      require('../lib/clean');
+    }
+  )
+  .command('*', 'the default command.',
+    () => {},
+    argv => {
+    console.log('Command not found. Run `react-power --help` for usage.')
+  })
   .help().argv;
