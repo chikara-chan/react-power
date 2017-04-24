e73e1c23dc4798b6306c10959c63009756beb9f3
chikara <chenhongtu@51xianqu.net>
2017-04-24 10:14:52 +0800

    feat: concat user babelrc

--- config/babel.config.js
+++ config/babel.config.js
@@ -1,23 +1,21 @@
-const deepAssign = require('deep-assign')
-const config = require('../lib/loadConfig')
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  var map = {}
 
-module.exports = deepAssign(
-  {
-    cacheDirectory: true,
-    presets: [
-      [
-        'env',
-        {
-          targets: {
-            browsers: config.browserslist
-          },
-          modules: false,
-          useBuiltIns: true
-        }
-      ],
-      'react'
-    ],
-    plugins: ['react-hot-loader/babel']
-  },
-  config.babelrc
-)
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] > target) {
+      continue
+    }
+
+    if (map[target - nums[i]] != undefined) {
+      return [map[target - nums[i]], i]
+    }
+    map[nums[i]] = i
+  }
+}
+
+console.log(twoSum([0, 4, 3, 0], 0))
