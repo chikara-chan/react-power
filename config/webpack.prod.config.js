bbbdd4568628a9fce7845642e48aece04bb9d636
chikara <chenhongtu@51xianqu.net>
2017-04-28 11:08:47 +0800

    refactor: remove stylus loader

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -62,29 +62,6 @@ module.exports = merge(base, {
         })
       },
       {
-        test: /\.styl$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                modules: config.cssModules,
-                camelCase: true,
-                importLoaders: 2,
-                localIdentName: '[hash:base64:8]'
-              }
-            },
-            {
-              loader: 'postcss-loader'
-            },
-            {
-              loader: 'stylus-loader'
-            }
-          ]
-        })
-      },
-      {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
