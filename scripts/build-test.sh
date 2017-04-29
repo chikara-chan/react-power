285c007f70100b2b799cfd8eb54aec3b40dca02d
chikara <chenhongtu@51xianqu.net>
2017-04-29 10:51:27 +0800

    test: add new test case for complex example

--- scripts/build-test.sh
+++ scripts/build-test.sh
@@ -5,17 +5,22 @@ set -e
 PATH=$(npm bin):$PATH
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 
-cd $ROOT_DIR/examples/basic
+cd $ROOT_DIR/examples/0-basic
 npm install
 npm run build
 npm run clean
 
-cd $ROOT_DIR/examples/multi
+cd $ROOT_DIR/examples/1-multi
 npm install
 npm run build
 npm run clean
 
-cd $ROOT_DIR/examples/spa
+cd $ROOT_DIR/examples/2-spa
+npm install
+npm run build
+npm run clean
+
+cd $ROOT_DIR/examples/3-complex
 npm install
 npm run build
 npm run clean
