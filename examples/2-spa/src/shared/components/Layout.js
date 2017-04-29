e42dcdf5f3d6e76d662acb3d26f8155e2ff4fb7b
chikara <chenhongtu@51xianqu.net>
2017-04-29 10:00:10 +0800

    refactor: reconfig spa package.json

---
+++ examples/2-spa/src/shared/components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from './Layout.scss'
+
+function Layout(props) {
+  return (
+    <div className={styles.container}>
+      <ul>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/explore">Explore</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+      {props.children}
+    </div>
+  )
+}
+
+export default Layout
